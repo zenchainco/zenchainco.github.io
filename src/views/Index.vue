59d553fa0a5462b719a3a2046eed157758c9d076
Zen <web@zenchain.co>
2021-05-16 09:08:16 +0700

    Fix typo?

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -149,7 +149,7 @@
       <div class="container mx-auto px-4">
         <div class="flex flex-wrap justify-center text-center mb-5">
           <div class="w-full lg:w-6/12 px-4">
-            <h2 class="text-4xl font-semibold">POWER BY</h2>
+            <h2 class="text-4xl font-semibold">POWERED BY</h2>
           </div>
         </div>
         <div
